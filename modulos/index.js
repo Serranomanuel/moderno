814f491dc1ccdd5448c568d0648a750ddd6593d9
Serranomanuel <Serranomanuel106@gmail.com>
2025-12-06 19:24:57 -0500

    commit ejercicio 13: con sus respectivas funciones en carpeta modulo

--- modulos/index.js
+++ modulos/index.js
@@ -10,3 +10,4 @@ export { procesarPagos } from './funcionesEjer9.js';
 export {fusionarUsuarios} from './funcionesEjer10.js';
 export {generarResumen} from './funcionesEjer11.js';
 export {seleccionarPaciente} from './funcionesEjer12.js';
+export * from './funcionesEjer13.js';
